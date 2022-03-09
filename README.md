37847997f90f1ad84ee8403fb5e9c8930af14ca7
Travis Hatley <travis1010@gmail.com>
2022-03-08 18:24:45 -0700

    made links open in new tab

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Shopping Cart assignment for The Odin Project 
 https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/shopping-cart
+
+live site: https://stormy-inlet-58711.herokuapp.com/
--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,9 @@ function About() {
   return (
     <div className='About'>
       <h1>About</h1>
-      <div>This is a fake website made as an assignment for <a href='https://www.theodinproject.com/'>The Odin Project.</a></div>
-      <div><a href='https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/shopping-cart'>Assignment Page</a></div>
-      <div><a href='https://github.com/travis1010/shopping-cart'>Github Repo</a></div>
+      <div>This is a fake website made as an assignment for <a href='https://www.theodinproject.com/' target='_blank'>The Odin Project.</a></div>
+      <div><a href='https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/shopping-cart' target='_blank'>Assignment Page</a></div>
+      <div><a href='https://github.com/travis1010/shopping-cart' target='_blank'>Github Repo</a></div>
     </div>
   )
 }
