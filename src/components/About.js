64fe0b3504d537c12ddf84feb14211c132093f33
Travis Hatley <travis1010@gmail.com>
2022-03-08 18:20:21 -0700

    added repo link

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About() {
       <h1>About</h1>
       <div>This is a fake website made as an assignment for <a href='https://www.theodinproject.com/'>The Odin Project.</a></div>
       <div><a href='https://www.theodinproject.com/paths/full-stack-javascript/courses/javascript/lessons/shopping-cart'>Assignment Page</a></div>
-      <div><a href='#'>Github Repo</a></div>
+      <div><a href='https://github.com/travis1010/shopping-cart'>Github Repo</a></div>
     </div>
   )
 }
